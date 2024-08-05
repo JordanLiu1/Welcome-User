1a545fb4bf32d4947c064e8297f1c90a3e88cbd7
JordanLiu1 <1680jordan@gmail.com>
2024-08-05 17:55:18 +0000

    changes

---
+++ dom.js
@@ -0,0 +1,11 @@
+let username = prompt("Input your name")
+welcome = ("Hello " + username)
+document.querySelector('#text-output').innerText = welcome
+document.querySelector('#text-output').style.fontSize = '30px'
+document.body.style.cssText = 'color: white; background-color: black';
+let img document.createElement('img').src = 'https://th.bing.com/th/id/OIP.yStSLazi_rzqFBbXeXqUDQHaHa?w=177&h=180&c=7&r=0&o=5&pid=1.7';
+document.h1.append(img);
+let div = document.createElement("div");
+document.body.append(Mydiv);
+document.querySelector('Mydiv').innerText = 'hello world';
+document.remove(img)
